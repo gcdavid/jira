3bdc5a827cd16d867fef314613eab2d37a15f5ae
gcdavid <gcdavid17@gmail.com>
2024-10-18 18:03:46 -0400

    added sign in and sign up design

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,8 @@
+import { SignUpCard } from "@/app/features/auth/components/sign-up-card";
+import React from "react";
+
+const SignupPage = () => {
+  return <SignUpCard />;
+};
+
+export default SignupPage;
