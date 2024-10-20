a259f6a82aa84c5749c45246d006a85f1d836a25
gcdavid <gcdavid17@gmail.com>
2024-10-20 17:37:11 -0400

    fix: had two async calls which led to error in api

--- src/lib/session-middleware.ts
+++ src/lib/session-middleware.ts
@@ -28,32 +28,30 @@ type AdditionalContext = {
 };
 
 export const sessionMiddleware = createMiddleware<AdditionalContext>(
-  async (c) => {
-    async (c: any, next: any) => {
-      const client = new Client()
-        .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-        .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
+  async (c, next) => {
+    const client = new Client()
+      .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
+      .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
 
-      const session = getCookie(c, AUTH_COOKIE);
+    const session = getCookie(c, AUTH_COOKIE);
 
-      if (!session) {
-        return c.json({ error: "Unauthorized" }, 401);
-      }
+    if (!session) {
+      return c.json({ error: "Unauthorized" }, 401);
+    }
 
-      client.setSession(session);
+    client.setSession(session);
 
-      const account = new Account(client);
-      const databases = new Databases(client);
-      const storage = new Storage(client);
+    const account = new Account(client);
+    const databases = new Databases(client);
+    const storage = new Storage(client);
 
-      const user = await account.get();
+    const user = await account.get();
 
-      c.set("account", account);
-      c.set("databases", databases);
-      c.set("storage", storage);
-      c.set("user", user);
+    c.set("account", account);
+    c.set("databases", databases);
+    c.set("storage", storage);
+    c.set("user", user);
 
-      await next();
-    };
+    await next();
   }
 );
